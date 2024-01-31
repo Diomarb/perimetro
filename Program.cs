a37aa2f62d279e90c4a6d6c80bb4bb3a5a20b63b
arian <arian@DESKTOP-8TRUQ82>
2024-01-31 12:54:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class PerimetroDelCuadrado
+{
+    static void Main(string[] args)
+
+    {
+        try
+        {
+            //introducir el lado de el cuadrado//
+            Console.WriteLine("Coloque el valor del lado de el cuadrado:");
+
+            double lado = Convert.ToDouble(Console.ReadLine());
+            double perimetro = lado * 4;
+            //mostrar resultado//
+            Console.WriteLine("El perimetro de cuadrado es:" + perimetro);
+
+
+        }
+        catch (FormatException)
+        {
+            //manejo de exepcion para entrada no numerica//
+            Console.WriteLine("Error.Coloque un valor numerico");
+        }
+
+    }
+}
+            
+
